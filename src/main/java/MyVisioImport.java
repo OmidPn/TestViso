ce24660148789bca0bc327d1246bf48767c9f164
tmsvanier <omidpan4@gmail.com>
2016-12-10 00:36:38 -0500

    4th commit . resolve dependecy problem

--- src/main/java/MyVisioImport.java
+++ src/main/java/MyVisioImport.java
@@ -1,10 +1,13 @@
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xdgf.usermodel.XDGFPage;
+import org.apache.poi.xdgf.usermodel.XDGFShape;
 import org.apache.poi.xdgf.usermodel.XmlVisioDocument;
-
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -15,24 +18,37 @@ public class MyVisioImport {
 
     public static void  main (String[] args){
         try {
-//            POIDataSamples diagrams =  POIDataSamples.getDiagramInstance();
-            XmlVisioDocument xmlVisioDocument=null;
-            String fileName = "C:/Users/opanahi/Desktop/BPMN2.0project/justRec.vsdx";
-            File myfile = new File("C:/Users/opanahi/Desktop/BPMN2.0project/justRec.vsdx");
+            FileOutputStream fileOut = new FileOutputStream("C:\\Users\\omid\\Desktop\\poi-test.xls");
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet worksheet = workbook.createSheet("POI Worksheet");
+
+            //POIDataSamples diagrams =  POIDataSamples.getDiagramInstance();
+            XmlVisioDocument xmlViso=null;
+            String fileName = "C:\\Users\\omid\\Desktop\\exampleofBPMN2.vsdx";
+            File myfile = new File("C:/Users/opanahi/Desktop/BPMN2.0project/exampleofBPMN2.vsdx");
             OPCPackage opcPackage=OPCPackage.open(new FileInputStream((fileName)));
 
-           xmlVisioDocument = new XmlVisioDocument(opcPackage);
-            if(xmlVisioDocument!=null)
-            {
-                System.out.printf("test");
+           xmlViso= new XmlVisioDocument(opcPackage);
+            int n=0;
+            for (XDGFPage page: xmlViso.getPages()){
+                System.out.println(page.getPageSheet().getDocument().getXmlObject().getStyleSheets().getStyleSheetList().toString());
+                System.out.println("end of pege end of page end of page end of page:"+n);
+                n++;
             }
-         Iterator<XDGFPage> it=xmlVisioDocument.getPages().iterator();
+
+             XDGFShape shape=null;
+
+            if(xmlViso!=null)
+            {
+                System.out.println("test");
+            }
+         Iterator<XDGFPage> it=xmlViso.getPages().iterator();
 
             while (it.hasNext()){
-                System.out.printf(it.next().getName());
+                System.out.println(it.next().getName());
             }
 
-            System.out.println(xmlVisioDocument.toString());
+            System.out.println(xmlViso.toString());
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InvalidFormatException e) {
